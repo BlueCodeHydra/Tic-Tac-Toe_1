143938f869bd93ea42bab55cb3c231364c8ac480
Mike <113725850+BlueCodeHydra@users.noreply.github.com>
2023-11-18 19:00:49 -0500

    commit

--- TicTacToeAI.java
+++ TicTacToeAI.java
@@ -75,18 +75,71 @@ public class TicTacToeAI {
                 if (board[row][col] == ' ') {
                     board[row][col] = 'X';
                     if (evaluate(board) == -10) {
-                        board[row][col] = ' '; // Reset the test move
-                        return new int[]{row, col}; // Block player's winning move
+                        board[row][col] = 'O'; // Block player's winning move by placing 'O' here
+                        return new int[]{row, col};
                     }
                     board[row][col] = ' '; // Reset the test move
                 }
             }
         }
     
+        // Prioritize getting a winning move if available
+        int[] winningMove = getWinningMove(board);
+        if (winningMove != null) {
+            return winningMove;
+        }
+    
         // If no immediate winning or blocking moves are available,
         // prioritize getting a strategic move
         return getStrategicMove(board);
     }
+    
+    private int[] getWinningMove(char[][] board) {
+        // Check rows for a winning move
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][0] == 'O' && board[i][2] == ' ') {
+                return new int[]{i, 2};
+            }
+            if (board[i][1] == board[i][2] && board[i][1] == 'O' && board[i][0] == ' ') {
+                return new int[]{i, 0};
+            }
+            if (board[i][0] == board[i][2] && board[i][0] == 'O' && board[i][1] == ' ') {
+                return new int[]{i, 1};
+            }
+        }
+    
+        // Check columns for a winning move
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == board[1][i] && board[0][i] == 'O' && board[2][i] == ' ') {
+                return new int[]{2, i};
+            }
+            if (board[1][i] == board[2][i] && board[1][i] == 'O' && board[0][i] == ' ') {
+                return new int[]{0, i};
+            }
+            if (board[0][i] == board[2][i] && board[0][i] == 'O' && board[1][i] == ' ') {
+                return new int[]{1, i};
+            }
+        }
+    
+        // Check diagonals for a winning move
+        if (board[0][0] == board[1][1] && board[0][0] == 'O' && board[2][2] == ' ') {
+            return new int[]{2, 2};
+        }
+        if (board[1][1] == board[2][2] && board[1][1] == 'O' && board[0][0] == ' ') {
+            return new int[]{0, 0};
+        }
+        if (board[0][2] == board[1][1] && board[0][2] == 'O' && board[2][0] == ' ') {
+            return new int[]{2, 0};
+        }
+        if (board[1][1] == board[2][0] && board[1][1] == 'O' && board[0][2] == ' ') {
+            return new int[]{0, 2};
+        }
+    
+        return null; // No immediate winning move found
+    }
+    
+    
+    
     
 
     private static int evaluate(char[][] board) {
